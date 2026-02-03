2b2b6e0ca93cf6a66a766bcefc7b35731bad198e
Adam Good <dev.adamgood@gmail.com>
2026-02-03 15:14:38 -0500

    fixed type annotation

--- prototype/src/utils/math.py
+++ prototype/src/utils/math.py
@@ -1,3 +1,4 @@
+from typing import Callable
 import numpy as np
 
 def rad2deg(theta: float) -> float:
@@ -6,7 +7,7 @@ def rad2deg(theta: float) -> float:
 def deg2rad(theta: float) -> float:
     return theta * np.pi / 180
 
-def vec2d_to_angle(x: float, y: float, transform_fn: callable = lambda x: x) -> float:
+def vec2d_to_angle(x: float, y: float, transform_fn: Callable[[float], float] = lambda x: x) -> float:
     angle = np.atan2(y, x)
     angle = transform_fn(angle)
     angle = rad2deg(angle)
