cd58a39e2f0f522de4adb135a14add077a0a2e56
Adam Good <dev.adamgood@gmail.com>
2026-02-09 18:57:19 -0500

    updated type hints with q_vector

--- prototype/src/quantum/qubit.py
+++ prototype/src/quantum/qubit.py
@@ -1,14 +1,16 @@
-from quantum.gate import hgate, XGate, xgate
+from quantum.gate import hgate, xgate
 import dataclasses
 import numpy as np
 from utils.math import vec2d_to_angle
 
-def _ket0_state_factory():
+type q_vector = np.typing.NDArray[np.float64]
+
+def _ket0_state_factory() -> q_vector:
     return np.array([1.0, 0.0])
 
 @dataclasses.dataclass(frozen=True)
 class QuantumState:
-    state_vec: np.ndarray = dataclasses.field(default_factory=_ket0_state_factory)
+    state_vec: q_vector = dataclasses.field(default_factory=_ket0_state_factory)
 
     def __post__init__(self):
         # TODO: Implement Proper Errors
@@ -21,28 +23,31 @@ class QuantumState:
         else:
             return False
 
-    def to_vector(self) -> np.ndarray:
+    @property
+    def to_vector(self) -> q_vector:
         return self.state_vec
 
     @property
-    def _x(self) -> float:
+    def _x(self) -> np.float64:
         return self.state_vec[0]
 
     @property
-    def _y(self) -> float:
+    def _y(self) -> np.float64:
         return self.state_vec[1]
 
     @property
-    def angle(self) -> float:
-        return vec2d_to_angle(self._x, self._y)
+    def angle(self) -> np.float64:
+        angle = vec2d_to_angle(self._x, self._y)
+        return np.float64(angle)
 
     @property
-    def bloch_angles(self) -> float:
-        return vec2d_to_angle(self._x, self._y, lambda x: 2 * x)
+    def bloch_angles(self) -> np.float64:
+        angle = vec2d_to_angle(self._x, self._y, lambda x: 2 * x)
+        return np.float64(angle)
 
     @property
-    def probability_distribution(self) -> np.ndarray:
-        probabilities = np.abs(self.state_vec) ** 2
+    def probability_distribution(self) -> q_vector:
+        probabilities: q_vector = np.abs(self.state_vec) ** 2
         return probabilities
 
 
@@ -51,7 +56,7 @@ class Qubit:
         self.state = state
 
     def _collapse(self):
-        probabilities = self.state.probability_distribution
+        probabilities: q_vector = self.state.probability_distribution
         outcome_idx = np.random.choice([0,1], p=probabilities)
         new_state = np.zeros(shape=(2,))
         new_state[outcome_idx] = 1.0
@@ -63,15 +68,15 @@ class Qubit:
         return self.state
 
     def hadamard(self) -> Qubit:
-        vector = self.state.to_vector()
+        vector = self.state.to_vector
         state_vec = hgate(vector)
         new_state = QuantumState(state_vec)
         self.state = new_state
         return self
 
     def negate(self) -> Qubit:
-        vector = self.state.to_vector()
-        state_vec = xgate(vector)
+        vector = self.state.to_vector
+        state_vec: q_vector = xgate(vector)
         new_state = QuantumState(state_vec)
         self.state = new_state
         return self
