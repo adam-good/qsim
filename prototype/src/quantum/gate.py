29ce4e01b1388d0b1fc3e4a91605f72d5ce8aa69
Adam Good <dev.adamgood@gmail.com>
2026-02-09 17:42:02 -0500

    abstracting gate math out for other uses

--- prototype/src/quantum/gate.py
+++ prototype/src/quantum/gate.py
@@ -1,26 +1,32 @@
 from quantum.qubit import Qubit, QuantumState
 import numpy as np
 
+def hgate(vector: np.ndarray) -> np.ndarray:
+    matrix = np.array([
+        [1,1],
+        [1,-1]
+    ]) / np.sqrt(2)
+    return matrix @ vector
+
+def xgate(vector: np.ndarray) -> np.ndarray:
+    matrix = np.array([
+            [0,1],
+            [1,0]
+        ])
+    return matrix @ vector
+
 class Gate:
     def __call__(self, qubit: Qubit) -> np.ndarray:
         raise NotImplementedError("Subclasses must implement this method")
 
 class HGate(Gate):
     def __call__(self, qubit: Qubit) -> np.ndarray:
-        h_gate = np.array([
-                          [1, 1],
-                          [1, -1]
-                      ]) / np.sqrt(2)
-        new_state = h_gate @ qubit.state.to_vector()
+        new_state = hgate(qubit.state.to_vector())
         qubit.state = QuantumState(new_state)
         return new_state
 
 class XGate(Gate):
     def __call__(self, qubit: Qubit) -> np.ndarray:
-        x_gate = np.array([
-                          [0,1],
-                          [1,0]
-                      ])
-        new_state = x_gate @ qubit.state.to_vector()
+        new_state = xgate(qubit.state.to_vector())
         qubit.state = QuantumState(new_state)
         return new_state
