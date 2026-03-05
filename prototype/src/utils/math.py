33b6b5c195818a11fa8ad96a19f963c540e5d740
Adam Good <dev.adamgood@gmail.com>
2026-03-05 13:52:10 -0500

    updated math utils to use scalar type

--- prototype/src/utils/math.py
+++ prototype/src/utils/math.py
@@ -1,16 +1,17 @@
 from typing import Callable
 import numpy as np
+from utils.typing import scalar
 
-def rad2deg(theta: float) -> float:
-    return theta * 180 / np.pi
+def rad2deg(theta: scalar) -> scalar:
+    return scalar(theta * 180 / np.pi)
 
-def deg2rad(theta: float) -> float:
-    return theta * np.pi / 180
+def deg2rad(theta: scalar) -> scalar:
+    return scalar(theta * np.pi / 180)
 
-def vec2d_to_angle(x: float, y: float, transform_fn: Callable[[float], float] = lambda x: x) -> float:
+def vec2d_to_angle(x: scalar, y: scalar, transform_fn: Callable[[scalar], scalar] = lambda x: x) -> scalar:
     angle = np.atan2(y, x)
     angle = transform_fn(angle)
     angle = rad2deg(angle)
     while angle < 0 or angle > 360:
         angle = (angle + 360) % 360
-    return angle
+    return scalar(angle)
