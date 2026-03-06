970f97ba8eea9deb7584a27a27806abc3fe01e4b
Adam Good <dev.adamgood@gmail.com>
2026-03-06 11:32:53 -0500

    trying to fix type hinting

--- prototype/src/utils/typing.py
+++ prototype/src/utils/typing.py
@@ -1,6 +1,8 @@
-from utils.math import Matrix
+from typing import Annotated, Literal
 import numpy as np
+import numpy.typing as npt
 
 scalar = np.float64
-vector = np.typing.NDArray[scalar]
-matrix = Matrix # TODO: Can this be better defined?
+vector = Annotated[npt.NDArray[scalar], Literal[2]]
+matrix = Annotated[npt.NDArray[scalar], Literal['N', 'N']]
+
