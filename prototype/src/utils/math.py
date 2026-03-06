731bb153fd0f8c67ed966212997bfe51f67e2852
Adam Good <dev.adamgood@gmail.com>
2026-03-05 19:44:08 -0500

    creating matrix type

--- prototype/src/utils/math.py
+++ prototype/src/utils/math.py
@@ -1,6 +1,6 @@
 import numpy as np
-from typing import Callable
-from utils.typing import scalar
+from typing import Callable, overload
+from utils.typing import scalar, vector
 
 def rad2deg(theta: scalar) -> scalar:
     return scalar(theta * 180 / np.pi)
@@ -15,3 +15,47 @@ def vec2d_to_angle(x: scalar, y: scalar, transform_fn: Callable[[scalar], scalar
     while angle < 0 or angle > 360:
         angle = (angle + 360) % 360
     return scalar(angle)       
+
+class Matrix:
+    def __init__(self, vals: list[scalar] | vector):
+        if not isinstance(vals, vector):
+            vals = np.array(vals)
+        self._arr: vector = vals
+
+    def _is_square(self) -> bool:
+        return len(self._arr.shape) == 2 and self._arr.shape[0] == self._arr.shape[1]
+    
+
+    def _mat_mat_mul(self, other: Matrix) -> Matrix:
+        return Matrix(self._arr @ other._arr)
+
+    def _mat_vec_mul(self, other: vector) -> vector:
+        return self._arr @ other
+    
+    def __add__(self, other: Matrix) -> Matrix:
+        return Matrix(self._arr + other._arr)
+
+    def __sub__(self, other: Matrix) -> Matrix:
+        return Matrix(self._arr - other._arr)
+
+    def __mul__(self, other: Matrix) -> Matrix:
+        return Matrix(self._arr * other._arr)
+
+    def __div__(self, other: Matrix) -> Matrix:
+        return Matrix(self._arr / other._arr)
+
+    @overload
+    def __matmul__(self, other: Matrix) -> Matrix:
+        ...
+    @overload
+    def __matmul__(self, other: vector) -> vector:
+        ...
+    def __matmul__(self, other):
+        if isinstance(other, vector):
+            return self._mat_vec_mul(other)
+        elif isinstance(other, Matrix):
+            return self._mat_mat_mul(other)
+        else:
+            raise Exception("Undefined Other Type for Matrix Matmul")
+
+
--- prototype/src/utils/typing.py
+++ prototype/src/utils/typing.py
@@ -1,5 +1,6 @@
+from utils.math import Matrix
 import numpy as np
 
 scalar = np.float64
 vector = np.typing.NDArray[scalar]
-matrix = np.typing.NDArray[scalar] # TODO: Can this be better defined?
+matrix = Matrix # TODO: Can this be better defined?
