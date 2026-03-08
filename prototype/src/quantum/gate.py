fb9acbee6d7870829db4ce85a627b51df8f4c64f
Adam Good <dev.adamgood@gmail.com>
2026-03-07 20:27:42 -0500

    removed duplicate gate logic

--- prototype/src/quantum/gate.py
+++ prototype/src/quantum/gate.py
@@ -1,35 +1,43 @@
-from quantum.qubit import Qubit, QuantumState
-from utils.gates import H_GATE, X_GATE, is_unitary
+from utils.typing import matrix
+import quantum.state as qstate
 import numpy as np
-import numpy.typing as npt
 
-class Gate:
-    def __init__(self, matrix: npt.NDArray[np.float64]):
-        if not is_unitary(matrix):
-            raise Exception("Invalid Gate! Matrix Not Unitary")
-        self.matrix = matrix          
+type qgate = matrix
+
+def apply_gate(psi: qstate.state, gate: qgate, check_unitary: bool = True) -> qstate.state:
+    if check_unitary and not _is_unitary(gate):
+        raise Exception("Gate is Not Unitary")
+    return gate @ psi
+
+def hadamard(psi: qstate.state) -> qstate.state:
+    gate: qgate = np.matrix(
+        [[1,1],
+        [1,-1]]
+    ) / np.sqrt(2)   
+    return apply_gate(psi, gate, check_unitary=False)
+
+def xgate(psi: qstate.state) -> qstate.state:
+    gate = np.matrix(
+        [[0,1],
+        1,0]
+    )
+    return apply_gate(psi, gate, check_unitary=False)
+
+def negate(psi: qstate.state) -> qstate.state:
+    return xgate(psi)
+
+
+def _is_square(mat: matrix) -> bool:
+    return len(mat.shape) == 2 and mat.shape[0] == mat.shape[1]
+
+def _is_unitary(mat: matrix) -> bool:
+        if not _is_square(mat):
+            return False
+                
+        identity = np.identity(mat.shape[0])
+        # TODO: Add the conjugate part when we upgrade to complex numbers
+        conj_transpose = mat.transpose()
+
+        return np.allclose(mat @ conj_transpose, identity)
     
-    def __call__(self, qubit: Qubit) -> np.ndarray:
-        new_state = self.matrix @ qubit.state.vector
-        qubit.state = QuantumState(new_state)
-        return new_state
 
-    def __matmul__(self, other: Gate | Qubit | QuantumState) -> Gate | QuantumState:
-        if isinstance(other, Gate):
-            return Gate(self.matrix @ other.matrix)
-        elif isinstance(other, Qubit):
-            state = QuantumState(self.matrix @ other.state.vector)
-            other.state = state
-            return state
-        elif isinstance(other, QuantumState):
-            return QuantumState(self.matrix @ other.vector)
-              
-
-class HGate(Gate):
-    def __init__(self):
-        super().__init__(H_GATE)
-         
-
-class XGate(Gate):
-    def __init__(self):
-        super().__init__(X_GATE)
