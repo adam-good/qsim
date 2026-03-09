109d259ac12baa100c273a648dd9500df1b12281
Adam Good <dev.adamgood@gmail.com>
2026-03-09 16:38:50 -0400

    renamed types in math

--- prototype/src/utils/math.py
+++ prototype/src/utils/math.py
@@ -1,18 +1,18 @@
 import numpy as np
 from typing import Callable
-from utils.typing import scalar
+from utils.typing import Scalar
 
-def rad2deg(theta: scalar) -> scalar:
-    return scalar(theta * 180 / np.pi)
+def rad2deg(theta: Scalar) -> Scalar:
+    return Scalar(theta * 180 / np.pi)
 
-def deg2rad(theta: scalar) -> scalar:
-    return scalar(theta * np.pi / 180)
+def deg2rad(theta: Scalar) -> Scalar:
+    return Scalar(theta * np.pi / 180)
 
-def vec2d_to_angle(x: scalar, y: scalar, transform_fn: Callable[[scalar], scalar] = lambda x: x) -> scalar:
+def vec2d_to_angle(x: Scalar, y: Scalar, transform_fn: Callable[[Scalar], Scalar] = lambda x: x) -> Scalar:
     angle = np.atan2(y, x)
     angle = transform_fn(angle)
     angle = rad2deg(angle)
     while angle < 0 or angle > 360:
         angle = (angle + 360) % 360
-    return scalar(angle)       
+    return Scalar(angle)       
 
