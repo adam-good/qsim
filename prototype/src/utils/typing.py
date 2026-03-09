893fc0dcdbf4edaed136cdc2d3bb439f3b335d8f
Adam Good <dev.adamgood@gmail.com>
2026-03-09 16:23:19 -0400

    renamed types to fit python conventions

--- prototype/src/utils/typing.py
+++ prototype/src/utils/typing.py
@@ -2,18 +2,18 @@ from typing import Annotated, Literal, TypeAlias
 import numpy as np
 import numpy.typing as npt
 
-scalar: TypeAlias = npt.DTypeLike[np.float64]
-vector: TypeAlias = Annotated[npt.NDArray[scalar], Literal[2]]
-matrix: TypeAlias = Annotated[npt.NDArray[scalar], Literal['M', 'N']]
+Scalar: TypeAlias = npt.DTypeLike[np.float64]
+Vector: TypeAlias = Annotated[npt.NDArray[Scalar], Literal[2]]
+matrix: TypeAlias = Annotated[npt.NDArray[Scalar], Literal['M', 'N']]
 
-def to_vector(x: npt.ArrayLike) -> vector:
-    arr = np.asarray(x, dtype=scalar)
+def to_vector(x: npt.ArrayLike) -> Vector:
+    arr = np.asarray(x, dtype=Scalar)
     if arr.ndim != 1:
         raise Exception("Incorrect Array Shape for Vector")
     return arr
 
 def to_matrix(x: npt.ArrayLike) -> matrix:
-    arr = np.asarray(x, dtype=scalar)
+    arr = np.asarray(x, dtype=Scalar)
     if arr.ndim != 2:
         raise Exception("Incorrect Array Shape for Matrix")
     return arr
