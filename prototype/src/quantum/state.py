54072658d93726211db1964036b9f9fa1c9f0bc2
Adam Good <dev.adamgood@gmail.com>
2026-03-09 16:26:42 -0400

    redid typing in states

--- prototype/src/quantum/state.py
+++ prototype/src/quantum/state.py
@@ -1,9 +1,9 @@
 import numpy as np
+import utils.typing as types
 from utils.math import vec2d_to_angle, deg2rad
-from utils.typing import vector, scalar
 from enum import Enum
 
-type state = vector
+type QState = types.Vector
 
 class Basis(Enum):
     KET0 = 0
@@ -11,48 +11,48 @@ class Basis(Enum):
     KETPLUS = 2
     KETMINUS = 3
 
-BASE_STATES: dict[Basis, state] = {
+BASE_STATES: dict[Basis, QState] = {
     Basis.KET0:np.array([1.,0.]),
     Basis.KET1:np.array([0.,1.]),
     Basis.KETPLUS:np.array([1.,1.]) / np.sqrt(2),
     Basis.KETMINUS:np.array([1.,-1.]) / np.sqrt(2)
 }
 
-KET0: state = BASE_STATES[Basis.KET0]
-KET1: state = BASE_STATES[Basis.KET1]
-KETPLUS: state = BASE_STATES[Basis.KETPLUS]
-KETMINUS: state = BASE_STATES[Basis.KETMINUS]
+KET0: QState = BASE_STATES[Basis.KET0]
+KET1: QState = BASE_STATES[Basis.KET1]
+KETPLUS: QState = BASE_STATES[Basis.KETPLUS]
+KETMINUS: QState = BASE_STATES[Basis.KETMINUS]
 
-def _x(psi: state) -> scalar:
+def _x(psi: QState) -> types.Scalar:
     return psi[0]
 
-def _y(psi: state) -> scalar:
+def _y(psi: QState) -> types.Scalar:
     return psi[1]
 
-def is_valid(psi: state) -> bool:
+def is_valid(psi: QState) -> bool:
     return _x(psi)**2 + _y(psi)**2 == 1.0
 
-def angle(psi: state) -> scalar:
+def angle(psi: QState) -> types.Scalar:
     return vec2d_to_angle(_x(psi), _y(psi))
 
-def bloch_angle(psi: state) -> scalar:
+def bloch_angle(psi: QState) -> types.Scalar:
     return vec2d_to_angle(_x(psi), _y(psi),
                           lambda x: 2 * x)
 
 # NOTE: This might not be necessary anymore
 # Kinda redundant and all
-def to_vector(psi: state) -> vector:
+def to_vector(psi: QState) -> types.Vector:
     return psi
 
-def to_bloch_vector(psi: state) -> vector:
+def to_bloch_vector(psi: QState) -> types.Vector:
     angle = deg2rad(bloch_angle(psi))
-    return vector([np.cos(angle), np.sin(angle)])
+    return types.to_vector([np.cos(angle), np.sin(angle)])
 
-def probability_distribution(psi: state) -> vector:
+def probability_distribution(psi: QState) -> types.Vector:
     return np.abs(psi) ** 2
 
-def collapse(psi: state) -> state:
-    probabilities: vector = probability_distribution(psi)
+def collapse(psi: QState) -> QState:
+    probabilities: types.Vector = probability_distribution(psi)
     random_idx = np.random.choice([0,1], p=probabilities)
     result_states = [KET0, KET1]
     return result_states[random_idx]
