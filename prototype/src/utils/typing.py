082febf55342f7c27eba73763555e94ceb39fcf7
Adam Good <dev.adamgood@gmail.com>
2026-03-09 16:22:19 -0400

    added to_matrix and to_vector

--- prototype/src/utils/typing.py
+++ prototype/src/utils/typing.py
@@ -1,8 +1,19 @@
-from typing import Annotated, Literal
+from typing import Annotated, Literal, TypeAlias
 import numpy as np
 import numpy.typing as npt
 
-scalar = np.float64
-vector = Annotated[npt.NDArray[scalar], Literal[2]]
-matrix = Annotated[npt.NDArray[scalar], Literal['N', 'N']]
+scalar: TypeAlias = npt.DTypeLike[np.float64]
+vector: TypeAlias = Annotated[npt.NDArray[scalar], Literal[2]]
+matrix: TypeAlias = Annotated[npt.NDArray[scalar], Literal['M', 'N']]
 
+def to_vector(x: npt.ArrayLike) -> vector:
+    arr = np.asarray(x, dtype=scalar)
+    if arr.ndim != 1:
+        raise Exception("Incorrect Array Shape for Vector")
+    return arr
+
+def to_matrix(x: npt.ArrayLike) -> matrix:
+    arr = np.asarray(x, dtype=scalar)
+    if arr.ndim != 2:
+        raise Exception("Incorrect Array Shape for Matrix")
+    return arr
