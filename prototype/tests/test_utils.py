f357aa381fdaae62b5f8dbc638204d2d27fee1e5
Adam Good <dev.adamgood@gmail.com>
2026-03-19 10:15:41 -0400

    added tests for some of the math funcs

--- prototype/tests/test_utils.py
+++ prototype/tests/test_utils.py
@@ -1,5 +1,6 @@
-from matplotlib.pylab import result_type
+import utils.math as math
 from utils.typing import Vector, Matrix
+from math import pi
 import unittest
 
 class TestVector(unittest.TestCase):
@@ -151,32 +152,32 @@ class TestMatrixProperties(unittest.TestCase):
         result = matrix.transpose
         self.assertEqual(result, target)
 
-# class TestMathUtils(unittest.TestCase):
-#     def test_rad2deg(self):
-#         inputs = [0, pi/2, pi, 3*pi/2]
-#         targets = [0, 90, 180, 270]
-#         outputs = [qmath.rad2deg(x) for x in inputs]
-#         for (output, target) in zip(outputs,targets):
-#             self.assertEqual(output, target)
+class TestMathUtils(unittest.TestCase):
+    def test_rad2deg(self):
+        inputs = [0, pi/2, pi, 3*pi/2]
+        targets = [0, 90, 180, 270]
+        outputs = [math.rad2deg(x) for x in inputs]
+        for (output, target) in zip(outputs,targets):
+            self.assertEqual(output, target)
 
-#     def test_deg2rad(self):
-#         inputs = [0, 90, 180, 270]
-#         targets = [0, pi/2, pi, 3*pi/2]
-#         outputs = [qmath.deg2rad(x) for x in inputs]
-#         for (output, target) in zip(outputs, targets):
-#             self.assertEqual(output, target)
+    def test_deg2rad(self):
+        inputs = [0, 90, 180, 270]
+        targets = [0, pi/2, pi, 3*pi/2]
+        outputs = [math.deg2rad(x) for x in inputs]
+        for (output, target) in zip(outputs, targets):
+            self.assertEqual(output, target)
 
-#     def test_vec2d_to_angle(self):
-#         inputs = [
-#             [1,0],
-#             [0,1],
-#             [-1,0],
-#             [0,-1]
-#         ]
-#         targets = [0, 90, 180, 270]
-#         outputs = [qmath.vec2d_to_angle(x,y) for (x,y) in inputs]
-#         for (output, target) in zip(outputs, targets):
-#             self.assertEqual(output, target)
+    def test_vec2d_to_angle(self):
+        inputs = [
+            [1,0],
+            [0,1],
+            [-1,0],
+            [0,-1]
+        ]
+        targets = [0, 90, 180, 270]
+        outputs = [math.vec2d_to_angle(x,y) for (x,y) in inputs]
+        for (output, target) in zip(outputs, targets):
+            self.assertEqual(output, target)
 
 # class TestGateUtils(unittest.TestCase):
 #     def test_is_square(self):
