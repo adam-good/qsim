023367207dedcbd9c0b3e26c10b4fbf9cac8b319
Adam Good <dev.adamgood@gmail.com>
2026-03-26 11:44:46 -0400

    removed numpy deps

--- prototype/src/quantum/state.py
+++ prototype/src/quantum/state.py
@@ -1,4 +1,5 @@
-import numpy as np
+import math
+import random
 from utils.math.scalar import Scalar
 from utils.math.vector import Vector
 from utils.math.helper_funcs import vec2d_to_angle, deg2rad
@@ -15,8 +16,8 @@ class Basis(Enum):
 BASE_STATES: dict[Basis, QState] = {
     Basis.KET0 : QState((1.,0.)),
     Basis.KET1 : QState((0., 1.)),
-    Basis.KETPLUS: QState((1.,1.)) / np.sqrt(2),
-    Basis.KETMINUS: QState((1.,-1.)) / np.sqrt(2)
+    Basis.KETPLUS: QState((1.,1.)) / math.sqrt(2),
+    Basis.KETMINUS: QState((1.,-1.)) / math.sqrt(2)
 }
 
 KET0: QState = BASE_STATES[Basis.KET0]
@@ -47,15 +48,15 @@ def to_vector(psi: QState) -> Vector:
 
 def to_bloch_vector(psi: QState) -> Vector:
     angle = deg2rad(bloch_angle(psi))
-    return Vector((np.cos(angle), np.sin(angle)))
+    return Vector((math.cos(angle), math.sin(angle)))
 
 def probability_distribution(psi: QState) -> Vector:
     return psi ** 2
 
 def collapse(psi: QState) -> QState:
     probabilities: Vector = probability_distribution(psi)
-    random_idx = np.random.choice([0,1], p=probabilities.raw_data)
-    result_states = [KET0, KET1]
+    random_idx: int = random.choices([0,1], weights=probabilities.raw_data, k=1)[0]
+    result_states: list[Vector] = [KET0, KET1]
     return result_states[random_idx]
 
 def reset(_psi: QState | None = None) -> QState:
