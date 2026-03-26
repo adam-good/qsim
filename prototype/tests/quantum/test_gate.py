be67f039550a70e368594d9068fb3ede8060dfc0
Adam Good <dev.adamgood@gmail.com>
2026-03-26 14:10:16 -0400

    updated some test syntax

--- prototype/tests/quantum/test_gate.py
+++ prototype/tests/quantum/test_gate.py
@@ -1,20 +1,19 @@
 import unittest
-import math
 import quantum.state as qstate
 import quantum.gate as qgate
 
 class TestGates(unittest.TestCase):
     def test_h_gate(self):
-        qubit = qstate.KET0
-        result = qgate.hgate(qubit)
-        target = qstate.QState((1, 1)) / math.sqrt(2)
+        psi = qstate.KET0
+        result = qgate.hgate(psi)
+        target = qstate.KETPLUS
         self.assertEqual(result, target)
 
     def test_x_gate(self):
-        qubit = qstate.KET0
-        result = qgate.xgate(qubit)
-        target = qstate.QState((0,1))
-        self.assertEqual(result, target)
+        psi = qstate.KET0
+        result = qgate.xgate(psi)
+        target = qstate.KET1
+        self.assertEqual(result, target)       
 
 
 if __name__ == "__main__":
