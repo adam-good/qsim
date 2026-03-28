e890ed1009d0eae3447ab45ab3bb85a7aa01657f
Adam Good <dev.adamgood@gmail.com>
2026-03-28 11:08:13 -0400

    created basic channel utility

--- prototype/src/utils/channel.py
+++ prototype/src/utils/channel.py
@@ -1,6 +1,28 @@
+from typing import TypeVar
+from dataclasses import dataclass
 from queue import Queue
-from typing import NamedTuple
 
-class Channel[T](NamedTuple):
+@dataclass
+class ChannelEndpoint[T]:
     input: Queue[T]
     output: Queue[T]
+
+@dataclass
+class Channel[T]:
+    endpoint_A: ChannelEndpoint[T]
+    endpoint_B: ChannelEndpoint[T]
+
+
+def new_channel(chnl_type: TypeVar) -> Channel:
+    queue_a = Queue[chnl_type]()
+    queue_b = Queue[chnl_type]()
+    endpoint_a = ChannelEndpoint(queue_a, queue_b)
+    endpoint_b = ChannelEndpoint(queue_b, queue_a)
+    channel = Channel[chnl_type](endpoint_a, endpoint_b)
+    return channel
+
+def send[T](endpoint: ChannelEndpoint[T], data: T):
+    endpoint.output.put(data)
+
+def recv[T](endpoint: ChannelEndpoint[T]) -> T:
+    return endpoint.input.get()
