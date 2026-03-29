304daaf5820edeab0efd913c8cfc741a5cadb18e
Adam Good <dev.adamgood@gmail.com>
2026-03-29 07:58:06 -0400

    changed qrng return type

--- prototype/src/quantum/algorithms/qrng.py
+++ prototype/src/quantum/algorithms/qrng.py
@@ -1,8 +1,8 @@
 from quantum.device import QuantumDevice
 from quantum.state import QState, Z_BASIS
 
-def qrng(device: QuantumDevice, map: dict[QState, int]) -> int:
+def qrng(device: QuantumDevice, map: dict[QState, int]) -> list[int]:
     with device.alloc() as psi:
         psi.hadamard()
         psi, measurment = psi.measure(Z_BASIS)
-    return map[measurment]
+    return [map[measurment]]
