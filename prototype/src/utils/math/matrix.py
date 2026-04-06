f71b1a271ec24e069b324e2634abd6e2e0f1dbb5
Adam Good <dev.adamgood@gmail.com>
2026-04-06 15:22:41 -0400

    refactor: simplified matrix.py

--- prototype/src/utils/math/matrix.py
+++ prototype/src/utils/math/matrix.py
@@ -1,12 +1,13 @@
 import math
-from typing import Tuple, Callable, overload
+from typing import Tuple
 from dataclasses import dataclass
 from utils.math.scalar import Scalar
 from utils.math.vector import Vector
 
+
 @dataclass(frozen=True)
 class Matrix:
-    raw_data: Tuple[Tuple[Scalar, ...], ...] # 2D Tuple so it's efficient
+    raw_data: Tuple[Tuple[Scalar, ...], ...]
 
     @property
     def n_rows(self) -> int:
@@ -17,170 +18,99 @@ class Matrix:
         return len(self.raw_data[0]) if self.raw_data else 0
 
     @property
-    def shape(self) -> Tuple[int,int]:
-        #return (len(self.raw_data), len(self.raw_data[0])) if self.raw_data else (0,0)
+    def shape(self) -> Tuple[int, int]:
         return (self.n_rows, self.n_cols)
 
     def col_vectors(self) -> Tuple[Vector, ...]:
-        transpose  = tuple(zip(*self.raw_data))
-        return tuple(Vector(c) for c in transpose)
+        return tuple(Vector(c) for c in
+            zip(*self.raw_data) # This acts as the transpose
+        )
 
     def row_vectors(self) -> Tuple[Vector, ...]:
-        return tuple(Vector(c) for c in self.raw_data)
+        return tuple(Vector(r) for r in self.raw_data)
 
     def transpose(self) -> Matrix:
         return Matrix(tuple(zip(*self.raw_data)))
 
     def is_square(self) -> bool:
-        rows, cols = self.shape
-        return rows == cols
+        return self.n_rows == self.n_cols
 
     def is_unitary(self) -> bool:
-        if not self.is_square:
-            return False      
+        if not self.is_square():
+            return False
         identity = Matrix.identity(self.n_rows)
-        transpose = self.transpose()
-        # TODO: This needs to be the conjugate transpose with complex
-        return self @ transpose == identity and transpose @ self == identity 
+        transpose = self.transpose() # TODO: This needs to be the conjugate transpose
+        return self @ transpose == identity and transpose @ self == identity
+
+    @staticmethod
     def identity(size: int) -> Matrix:
         return Matrix(tuple(
-            tuple(1 if i==j else 0 for i in range(size))
-            for j in range(size))
-        )
-    
+            tuple(1 if i == j else 0 for i in range(size))
+            for j in range(size)
+        ))
+
     def _flatten(self) -> Tuple[Scalar, ...]:
         return tuple(x for row in self.raw_data for x in row)
 
-    ###
-    # Private Math Functions
-    ###
-        
-    def _elementwise_op(matrix_a: Matrix, matrix_b: Matrix, op: Callable[[Scalar, Scalar], Scalar]) -> Matrix:
+    def __add__(self, other: Matrix) -> Matrix:
+        if not isinstance(other, Matrix):
+            return NotImplemented
         return Matrix(tuple(
-            tuple(op(a,b) for (a,b) in zip(row_a, row_b))
-            for row_a,row_b in zip(matrix_a.raw_data, matrix_b.raw_data)
+            tuple(a + b for a, b in zip(row_a, row_b))
+            for row_a, row_b in zip(self.raw_data, other.raw_data)
         ))
 
-    def _elementwise_scalar_op(matrix: Matrix, scalar: Scalar, op: Callable[[Scalar, Scalar], Scalar]) -> Matrix:
+    def __sub__(self, other: Matrix) -> Matrix:
+        if not isinstance(other, Matrix):
+            return NotImplemented
         return Matrix(tuple(
-            tuple(op(a, scalar) for a in row)
-            for row in matrix.raw_data
+            tuple(a - b for a, b in zip(row_a, row_b))
+            for row_a, row_b in zip(self.raw_data, other.raw_data)
         ))
 
-    def _scalar_add(matrix: Matrix, scalar: Scalar) -> Matrix:
-        return Matrix._elementwise_scalar_op(matrix, scalar, lambda a,b: a+b)
-
-    def _scalar_sub(matrix: Matrix, scalar: Scalar) -> Matrix:
-        return Matrix._elementwise_scalar_op(matrix, scalar, lambda a,b: a-b)
-
-    def _scalar_mul(matrix: Matrix, scalar: Scalar) -> Matrix:
-        return Matrix._elementwise_scalar_op(matrix, scalar, lambda a,b: a*b)
-
-    def _scalar_div(matrix: Matrix, scalar: Scalar) -> Matrix:
-        return Matrix._elementwise_scalar_op(matrix, scalar, lambda a,b: a/b)
-
-    def _matrix_add(matrix_a: Matrix, matrix_b: Matrix) -> Matrix:
-        return Matrix._elementwise_op(matrix_a, matrix_b, lambda a,b: a+b)
-
-    def _matrix_sub(matrix_a: Matrix, matrix_b: Matrix) -> Matrix:
-        return Matrix._elementwise_op(matrix_a, matrix_b, lambda a,b: a-b)
-
-    def _matrix_mul(self, matrix: Matrix) -> Matrix:
-        return self._elementwise_op(matrix, lambda a,b: a*b)
-
-    def _matrix_div(matrix_a: Matrix, matrix_b: Matrix) -> Matrix:
-        return Matrix._elementwise_op(matrix_a, matrix_b, lambda a,b: a/b)
-
-    def _vector_matmul(matrix: Matrix, vector: Vector) -> Vector:
-        rows, cols = matrix.shape
-        if rows != len(vector):
-            raise Exception("MatVecMul incompatiable sizes")
-        return Vector(tuple(
-            Vector.dotprod(row, vector) for row in matrix.row_vectors()
-        ))
-
-    def _matrix_matmul(a: Matrix, b: Matrix) -> Matrix:
-        a_rows, a_cols = a.shape
-        b_rows, b_cols = b.shape
-        if a_cols != b_rows:
-            raise Exception("Matrix Matmul Incomatible Matrix Shapes")
+    def __mul__(self, other: Scalar) -> Matrix:
+        if not isinstance(other, (int, float)):
+            return NotImplemented
         return Matrix(tuple(
-            tuple(Vector.dotprod(w,v) for v in b.col_vectors()) for w in a.row_vectors()
+            tuple(a * other for a in row)
+            for row in self.raw_data
         ))
 
-    ###
-    # Math Overloads
-    ###
-    @overload
-    def __add__(self, other: Scalar) -> Matrix: ...
-    @overload
-    def __add__(self, other: Matrix) -> Matrix: ...
-    @overload
-    def __sub__(self, other: Scalar) -> Matrix: ...
-    @overload
-    def __sub__(self, other: Matrix) -> Matrix: ...
-    @overload
-    def __mul__(self, other: Scalar) -> Matrix: ...
-    @overload
-    def __mul__(self, other: Matrix) -> Matrix: ...
-    @overload
-    def __truediv__(self, other: Scalar) -> Matrix: ...
-    @overload
-    def __truediv__(self, other: Matrix) -> Matrix: ...
-    @overload
-    def __matmul__(self, other: Vector) -> Vector: ...
-    @overload
-    def __matmul__(self, other: Matrix) -> Matrix: ...
+    def __rmul__(self, other: Scalar) -> Matrix:
+        return self.__mul__(other)
 
-    ###
-    # Concrete Operator Overload Implemetations
-    ###
-    def __add__(self, other: Matrix | Scalar) -> Matrix:
-        if isinstance(other, Matrix):
-            return Matrix._matrix_add(self, other)
-        elif isinstance(other, Scalar):
-            return Matrix._scalar_add(self, other)
-        else:
-            raise NotImplementedError()
+    def __truediv__(self, other: Scalar) -> Matrix:
+        if not isinstance(other, (int, float)):
+            return NotImplemented
+        return Matrix(tuple(
+            tuple(a / other for a in row)
+            for row in self.raw_data
+        ))
 
-    def __sub__(self, other: Matrix | Scalar) -> Matrix:
-        if isinstance(other, Matrix):
-            return Matrix._matrix_sub(self, other)
-        elif isinstance(other, Scalar):
-            return Matrix._scalar_sub(self, other)
-        else:
-            raise NotImplementedError()
-    
-    def __mul__(self, other: Matrix) -> Matrix:
-        if isinstance(other, Matrix):
-            return Matrix._matrix_mul(self, other)
-        elif isinstance(other, Scalar):
-            return Matrix._scalar_mul(self, other)
-        else:
-            raise NotImplementedError()
-
-    
-    def __truediv__(self, other: Matrix) -> Matrix:
-        if isinstance(other, Matrix):
-            return Matrix._matrix_div(self, other)
-        elif isinstance(other, Scalar):
-            return Matrix._scalar_div(self, other)
-        else:
-            raise NotImplementedError()
-
+    # TODO: Clean this up
     def __matmul__(self, other: Matrix | Vector) -> Matrix | Vector:
         if isinstance(other, Matrix):
-            return Matrix._matrix_matmul(self, other)
+            if self.n_cols != other.n_rows:
+                raise ValueError("Matrix matmul incompatible shapes")
+            return Matrix(tuple(
+                tuple(Vector.dotprod(w, v) for v in other.col_vectors())
+                for w in self.row_vectors()
+            ))
         elif isinstance(other, Vector):
-            return Matrix._vector_matmul(self, other)
-        else:
-            raise NotImplementedError("Not Implemented For Type")
+            if self.n_cols != len(other):
+                raise ValueError("Matrix-vector matmul incompatible sizes")
+            return Vector(tuple(
+                Vector.dotprod(row, other) for row in self.row_vectors()
+            ))
+        return NotImplemented
 
     def __eq__(self, other: object) -> bool:
-        if isinstance(other, Matrix):
-            for (a,b) in zip(self._flatten(), other._flatten()):
-                if not math.isclose(a,b):
-                    return False
-            return True
-        else:
-            raise NotImplementedError()
+        if not isinstance(other, Matrix):
+            return NotImplemented
+        return all(
+            math.isclose(a, b)
+            for a, b in zip(self._flatten(), other._flatten())
+        )
+
+    def __repr__(self):
+        return f"{self.raw_data}"
