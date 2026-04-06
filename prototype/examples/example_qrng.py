b020f4064f12d0a4abd958353b6072cc14460b40
Adam Good <dev.adamgood@gmail.com>
2026-04-05 20:42:09 -0400

    refactor: qrng example

--- prototype/examples/example_qrng.py
+++ prototype/examples/example_qrng.py
@@ -1,10 +1,10 @@
 from quantum.algorithms.qrng import qrng
 import quantum.simulation as qsim
-import quantum.state as qstate
 
 def main():
     n_qubits = 4
-    device = qsim.SimDevice([qsim.SimQubit(ref_id) for ref_id in range(n_qubits)])
+    qubits = [qsim.SimQubit(ref_id) for ref_id in range(n_qubits)]
+    device = qsim.SimDevice(qubits)
     result = qrng(16, device)
     print(result)
 
