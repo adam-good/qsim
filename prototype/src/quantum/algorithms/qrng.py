9cebff9e50b76230e016aefcf96addf7b587536a
Adam Good <dev.adamgood@gmail.com>
2026-04-07 08:50:35 -0400

    refactor: more qrng cleanup

--- prototype/src/quantum/algorithms/qrng.py
+++ prototype/src/quantum/algorithms/qrng.py
@@ -11,11 +11,10 @@ def qrng(
     result = n*[2]
     i = 0
     while i < n:
-        n_qubits = min(device.n_available_qubits(), n - i)
-        with device.n_alloc(n_qubits) as qubits:
+        batch_size = min(device.n_available_qubits(), n - i)
+        with device.n_alloc(batch_size) as qubits:
             for qubit in qubits:
-                qubit = qubit.hadamard()
-                qubit, measurment = qubit.measure(qstate.Z_BASIS)
+                _, measurment = qubit.hadamard().measure(qstate.Z_BASIS)
                 result[i] = state_map[measurment]
                 i += 1
     return result
