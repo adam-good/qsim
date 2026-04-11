7f48b5242536859d9d44a65cfe395b8e4d55cdad
Adam Good <dev.adamgood@gmail.com>
2026-04-11 13:29:21 -0400

    refactor: made it more simple

--- prototype/src/quantum/algorithms/qrng.py
+++ prototype/src/quantum/algorithms/qrng.py
@@ -1,18 +1,25 @@
 import quantum.device as qdev
 import quantum.state as qstate
 
+def _qubit_to_bit(state: qstate.QState) -> int:
+    return 0 if state == qstate.KET0 else 1
+
+def _generate_random_bits(n: int, device: qdev.QuantumDevice) -> list[int]:
+    assert n <= device.n_available_qubits()
+
+    with device.n_alloc(n) as qubits:
+        return [_qubit_to_bit(state) for _,state in
+            [qubit.hadamard().measure(qstate.Z_BASIS) for qubit in qubits]
+        ]
 
 def qrng(
     n: int, device: qdev.QuantumDevice,
 ) -> list[int]:
 
-    result = [0] * n
+    result = []
     i = 0
     while i < n:
         batch_size = min(device.n_available_qubits(), n - i)
-        with device.n_alloc(batch_size) as qubits:
-            for qubit in qubits:
-                _, measurement = qubit.hadamard().measure(qstate.Z_BASIS)
-                result[i] = 0 if measurement == qstate.KET0 else 1
-                i += 1
+        result.extend(_generate_random_bits(batch_size, device))
+        i += batch_size
     return result
