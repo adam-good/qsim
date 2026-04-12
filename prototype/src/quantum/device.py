c75b59be9e7c2493996f68b88f02384b925b9a91
Adam Good <dev.adamgood@gmail.com>
2026-04-12 12:52:08 -0400

    feature: qubit transfer between devices

--- prototype/src/quantum/device.py
+++ prototype/src/quantum/device.py
@@ -44,6 +44,18 @@ class QuantumDevice(metaclass=ABCMeta):
         pass
 
     @abstractmethod
+    def pop_qubit(self, qubit: Qubit) -> Qubit:
+        pass
+
+    @abstractmethod
+    def push_qubit(self, qubit: Qubit):
+        pass
+
+    @abstractmethod
+    def transfer(self, device: QuantumDevice, qubit: Qubit):
+        pass
+
+    @abstractmethod
     def _alloc(self) -> Qubit:
         pass
 
