e2bbc72c67109538359c446f2cdd94a640125679
Adam Good <dev.adamgood@gmail.com>
2026-04-13 16:29:36 -0400

    refactor: stripped methods and renamed classes

--- prototype/src/quantum/device.py
+++ prototype/src/quantum/device.py
@@ -1,11 +1,9 @@
-from dataclasses import dataclass
-import quantum.device as qdev
+import dataclasses as dcls
 import quantum.state as qstate
 import quantum.gate as qgate
 
-
-@dataclass(frozen=True, eq=False)
-class SimQubit(qdev.Qubit):
+@dcls.dataclass
+class Qubit:
     id: int = 0
     state: qstate.QState = qstate.KET0
 
@@ -13,37 +11,25 @@ class SimQubit(qdev.Qubit):
     def ref_id(self) -> int:
         return self.id
 
-    def reset(self) -> SimQubit:
-        return SimQubit(self.id, qstate.KET0)
+    def reset(self) -> Qubit:
+        return Qubit(self.id, qstate.KET0)
 
-    def measure(
-        self, basis: tuple[qstate.QState, qstate.QState]
-    ) -> tuple[SimQubit, qstate.QState]:
-        collapsed = qstate.collapse(basis, self.state)
-        return (SimQubit(self.id, collapsed), collapsed)
-
-    def hadamard(self) -> SimQubit:
-        return SimQubit(self.id, qgate.hadamard(self.state))
-
-    def negate(self) -> SimQubit:
-        return SimQubit(self.id, qgate.negate(self.state))
-
-    def ref_eq(self, other: qdev.Qubit) -> bool:
+    def ref_eq(self, other: Qubit) -> bool:
         return self.id == other.ref_id
 
     def state_eq(self, state: qstate.QState) -> bool:
         return self.state == state
 
-    def equiv(self, other: SimQubit) -> bool:
+    def equiv(self, other: Qubit) -> bool:
         return self.state == other.state
 
     def __repr__(self) -> str:
         return f"SimQubit({self.id}, {self.state})"
 
 
-class SimDevice(qdev.QuantumDevice):
-    def __init__(self, qubits: list[SimQubit]):
-        self.qubits: dict[int, qdev.Qubit] = {qubit.id: qubit for qubit in qubits}
+class QuantumDevice:
+    def __init__(self, qubits: list[Qubit]):
+        self.qubits: dict[int, Qubit] = {qubit.id: qubit for qubit in qubits}
         self.allocated: set[int] = set()
 
     def _available(self) -> set[int]:
@@ -52,17 +38,17 @@ class SimDevice(qdev.QuantumDevice):
     def n_available_qubits(self) -> int:
         return len(self.qubits) - len(self.allocated)
 
-    def _n_alloc(self, n: int) -> list[qdev.Qubit]:
+    def _n_alloc(self, n: int) -> list[Qubit]:
         assert n <= self.n_available_qubits()
 
         selection: list[int] = list(self._available())[:n]
-        qubits: list[qdev.Qubit] = [self.qubits[i] for i in selection]
+        qubits: list[Qubit] = [self.qubits[i] for i in selection]
         self.allocated.update(selection)
         return qubits
 
-    def _alloc(self) -> qdev.Qubit:
+    def _alloc(self) -> Qubit:
         return self._n_alloc(1)[0]
 
-    def _dealloc(self, qubit: qdev.Qubit):
+    def _dealloc(self, qubit: Qubit):
         self.allocated.remove(qubit.ref_id)
         self.qubits[qubit.ref_id] = qubit
