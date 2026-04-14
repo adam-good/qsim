a069d2b9cf05586660ca667ea0fa165dafb034bb
Adam Good <dev.adamgood@gmail.com>
2026-04-14 16:56:51 -0400

    updated example syntax

--- prototype/examples/example_quantum_state.py
+++ prototype/examples/example_quantum_state.py
@@ -1,22 +1,17 @@
 # prototype/examples/example_quantum_state.py
-
+import utils.math.vector
 import math
 import quantum.state as qstate
-import quantum.simulation as qsim
 
 
 def main():
     # Create a custom quantum state |ψ⟩ = (1/√2)|0> + (1/√2)|1>
-    state_vector = (1 / math.sqrt(2), 1 / math.sqrt(2))
-    custom_state = qstate.qstate_from_data(state_vector)
+    state_vector = utils.math.vector.Vector((1 / math.sqrt(2), 1 / math.sqrt(2)))
+    custom_state = qstate.QState(state_vector)
 
-    # Create a qubit with the custom state
-    qubit = qsim.SimQubit(0, custom_state)
-
-    # Measure the qubit
-    qubit, outcome = qubit.measure(qstate.Z_BASIS)
-    print(f"Measurement Outcome: {outcome}")
-    print(f"Post-Measurment State: {qubit}")
+    # Collapse (such as when it is measured) quantum state
+    outcome = qstate.collapse( qstate.Z_BASIS, custom_state)
+    print(f"Outcome: {outcome}")
 
 
 if __name__ == "__main__":
