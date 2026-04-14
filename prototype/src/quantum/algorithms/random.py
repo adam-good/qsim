bc23e3cf285d66313beeb7124c6ced23d0b3b8ba
Adam Good <dev.adamgood@gmail.com>
2026-04-14 14:16:07 -0400

    updated qrand syntax

--- prototype/src/quantum/algorithms/random.py
+++ prototype/src/quantum/algorithms/random.py
@@ -1,21 +1,34 @@
+import quantum.gate as qgate
 import quantum.device as qdev
 import quantum.state as qstate
 
 
-def _qubit_to_bit(state: qstate.QState) -> int:
+def _qstate_to_bit(state: qstate.QState) -> int:
     return 0 if state == qstate.KET0 else 1
 
+def _qstates_to_bits(states: list[qstate.QState]) -> list[int]:
+    return [_qstate_to_bit(state) for state in states]
+
+def _prepare_qubits(qubits: list[qdev.Qubit], device: qdev.QuantumDevice) -> list[qdev.Qubit]:
+    return [
+        device.prepare_qubit(qubit, [qgate.H_GATE])
+        for qubit in qubits
+    ]
+
+def _measure_qubits(qubits: list[qdev.Qubit], device: qdev.QuantumDevice) -> list[qstate.QState]:
+    return [
+        device.measure_qubit(qubit, qstate.Z_BASIS)
+        for qubit in qubits
+    ]
 
 def _batch_random_bits(n: int, device: qdev.QuantumDevice) -> list[int]:
-    assert n <= device.n_available_qubits()
+    assert n <= device.n_available_qubits() # TODO: Don't use asserts like this!
 
-    with device.n_alloc(n) as qubits:
-        return [
-            _qubit_to_bit(state)
-            for _, state in [
-                qubit.hadamard().measure(qstate.Z_BASIS) for qubit in qubits
-            ]
-        ]
+    with device.alloc(n) as qubits:
+        prepared_qubits: list[qdev.Qubit] = _prepare_qubits(qubits, device)
+        measured_states: list[qstate.QState] = _measure_qubits(prepared_qubits, device)
+        bits: list[int] = [_qstate_to_bit(state) for state in measured_states]
+        return bits
 
 
 def generate_random_bits(
