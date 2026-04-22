0937523c85a3d0f409323ac2969653e7e6277f35
Adam Good <dev.adamgood@gmail.com>
2026-04-22 17:55:03 -0400

    added case for empty channel

--- prototype/src/utils/channel.py
+++ prototype/src/utils/channel.py
@@ -9,7 +9,9 @@ def send[T](chnl: Channel[T], data: T) -> Channel[T]:
     return Channel(chnl.values + (data,))
     return chnl
 
-def recv[T](chnl: Channel[T]) -> tuple[T, Channel[T]]:
+def recv[T](chnl: Channel[T]) -> tuple[T | None, Channel[T] | None]:
+    if not chnl.values:
+        return (None, None)
     return (
         chnl.values[0],
         Channel(chnl.values[1:])
