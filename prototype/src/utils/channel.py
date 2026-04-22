bac0fb65b5b3f5b6693dc8a975e8ec243d85f8d2
Adam Good <dev.adamgood@gmail.com>
2026-04-22 17:50:56 -0400

    made channel immutable

--- prototype/src/utils/channel.py
+++ prototype/src/utils/channel.py
@@ -6,10 +6,11 @@ class Channel[T]:
     values: tuple[T,...] = ()
 
 def send[T](chnl: Channel[T], data: T) -> Channel[T]:
-    chnl.values: tuple[T,...] = chnl.values + (data,)
+    return Channel(chnl.values + (data,))
     return chnl
 
 def recv[T](chnl: Channel[T]) -> tuple[T, Channel[T]]:
-    data: T = chnl.values[0]
-    chnl.values: tuple[T,...] = chnl.values[1:]
-    return data, chnl
+    return (
+        chnl.values[0],
+        Channel(chnl.values[1:])
+    )
