ae0cfc96585b1a5146b3d060a905324733bbc514
Adam Good <dev.adamgood@gmail.com>
2026-05-03 20:23:50 -0400

    keep it simple stupid

--- prototype/src/utils/comms/channel.py
+++ prototype/src/utils/comms/channel.py
@@ -10,21 +10,3 @@ class Channel[T]:
 
     def recv(self) -> T:
         return self._queue.get()
-
-@dataclasses.dataclass
-class TwoWayChannel[T]:
-    _transmitter: queue.Queue[T] = queue.Queue[T]()
-    _reciever: queue.Queue[T] = queue.Queue[T]()
-
-    def send(self, data: T):
-        self._transmitter.put(data)
-
-    def recieve(self) -> T:
-        return self._reciever.get()
-
-def _compliment(channel: TwoWayChannel) -> TwoWayChannel:
-    return TwoWayChannel(channel._reciever, channel._transmitter)
-
-def channel_pair[T]() -> tuple[TwoWayChannel[T], TwoWayChannel[T]]:
-    channel = TwoWayChannel[T]()
-    return channel, _compliment(channel)
