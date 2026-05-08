cd037691affee749052f99428170a16397b13402
Adam Good <dev.adamgood@gmail.com>
2026-05-08 19:31:35 -0400

    added state constructor validation

--- QSimPrototype/src/Utils.jl
+++ QSimPrototype/src/Utils.jl
@@ -1,5 +1,9 @@
 module Utils
 
-Scalar = Float64 # NOTE: This will be updated to Complex later
+Scalar = AbstractFloat # NOTE: This will be updated to Complex later
+
+function SatisfiesBornRule(w::Vector)::Bool
+    return sum(x^2 for x in w) == 1.0
+end
 
 end
--- QSimPrototype/src/quantum/State.jl
+++ QSimPrototype/src/quantum/State.jl
@@ -1,10 +1,19 @@
 module Quantum
-using ..Utils: Scalar
+using ..Utils: Scalar, SatisfiesBornRule
 
 export State
 
 struct State
     vector::Vector{Scalar}
+
+    function State(vector::Vector)
+        vector = convert(Vector{Scalar}, vector)
+        if SatisfiesBornRule(vector)
+            return new(vector)
+        else
+            throw(DomainError("$vector does not satisfy Born Rule"))
+        end
+    end
 end
 
-end
+end # module Quantum
