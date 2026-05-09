c9e12ebd8c70df1a0a7e9a9611deb79e5206ea88
Adam Good <dev.adamgood@gmail.com>
2026-05-09 18:21:40 -0400

    Renamed to MathUtils

--- QSimPrototype/src/Utils.jl
+++ QSimPrototype/src/Utils.jl
@@ -1,6 +1,6 @@
-module Utils
+module MathUtils
 
-export Scalar, Angle, angle2d
+export Scalar, Angle, angle2d, dotprod, born_rule_constraint
 
 const Scalar::DataType = AbstractFloat # NOTE: This will be updated to Complex later
 
@@ -15,7 +15,7 @@ end
 
 Base.convert(::Type{Angle}, x::Real) = Angle(x)
 
-SatisfiesBornRule(w::Vector{Scalar})::Bool =  sum(x^2 for x in w) ≈ 1.0
+born_rule_constraint(w::Vector{Scalar})::Bool =  sum(x^2 for x in w) ≈ 1.0
 
 x(w::Vector{Scalar})::Scalar = w[1]
 y(w::Vector{Scalar})::Scalar = w[2]
@@ -24,5 +24,5 @@ angle2d(w::Vector)::Angle                              = angle2d(convert(Vector{
 angle2d(w::Vector, transform::Function)::Angle         = angle2d(convert(Vector{Scalar}, w), transform)
 angle2d(w::Vector{Scalar}, transform::Function)::Angle = transform(atand(y(w), x(w)))
 
-
+dotprod(w::Vector, v::Vector) = sum(w .* v)
 end
