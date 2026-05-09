bfc1fff24c0d81eab22f209fdee6f8c89e88fe5a
Adam Good <dev.adamgood@gmail.com>
2026-05-09 12:05:59 -0400

    Added some vector angle utilities

--- QSimPrototype/src/Utils.jl
+++ QSimPrototype/src/Utils.jl
@@ -1,9 +1,26 @@
 module Utils
 
-Scalar = AbstractFloat # NOTE: This will be updated to Complex later
+const Scalar::DataType = AbstractFloat # NOTE: This will be updated to Complex later
 
-function SatisfiesBornRule(w::Vector)::Bool
-    return sum(x^2 for x in w) == 1.0
+struct Angle
+    value::Scalar
+
+    function Angle(θ::Real)::Angle
+        θ = convert(Scalar, θ)
+        new( (θ + 360) % 360 ) 
+    end
 end
+
+Base.convert(::Type{Angle}, x::Real) = Angle(x)
+
+SatisfiesBornRule(w::Vector{Scalar})::Bool = sum(x^2 for x in w) == 1.0
+
+x(w::Vector{Scalar})::Scalar = w[1]
+y(w::Vector{Scalar})::Scalar = w[2]
+
+angle2d(w::Vector)::Angle                              = angle2d(convert(Vector{Scalar},w), x -> x)
+angle2d(w::Vector, transform::Function)::Angle         = angle2d(convert(Vector{Scalar}, w), transform)
+angle2d(w::Vector{Scalar}, transform::Function)::Angle = transform(atand(y(w), x(w)))
+
 
 end
