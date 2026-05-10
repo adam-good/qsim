e8e0b193d74b301f558388796e11a777ecbf92be
Adam Good <dev.adamgood@gmail.com>
2026-05-09 21:18:52 -0400

    WIP 2d Vector

--- QSimPrototype/src/Utils.jl
+++ QSimPrototype/src/Utils.jl
@@ -14,10 +14,32 @@ end
 Base.show(io::IO, x::Angle) = print(io, "$(x.value)°")
 Base.convert(::Type{Angle}, x::Real) = Angle(x)
 
-born_rule_constraint(w::Vector{Scalar})::Bool =  sum(x^2 for x in w) ≈ 1.0
+struct Vector2 <: AbstractVector{Scalar}
+    x::Scalar
+    y::Scalar 
+    function Vector2(w::Vector)
+        if length(w) != 2
+            throw(ErrorException("Vector2 Must be Length 2"))
+        end
+        return new(w[1],w[2])
+    end
+end
+Base.size(w::Vector2) = return (2,)
+Base.getindex(w::Vector2, i::Int) = begin
+    if i == 1
+        return w.x
+    elseif i == 2
+        return w.y
+    else
+        throw(ErrorException("Index Out of Range $i"))
+    end
+end
+Base.getindex(w::Vector2, I::Vararg{Int, 2}) = getindex(w, I) 
 
-x(w::Vector{Scalar})::Scalar = w[1]
-y(w::Vector{Scalar})::Scalar = w[2]
+born_rule_constraint(w::Vector2)::Bool =  sum(x^2 for x in w) ≈ 1.0
+
+x(w::Vector2)::Scalar = w[1]
+y(w::Vector2)::Scalar = w[2]
 
 angle2d(w::Vector)::Angle                              = angle2d(convert(Vector{Scalar},w), x -> x)
 angle2d(w::Vector, transform::Function)::Angle         = angle2d(convert(Vector{Scalar}, w), transform)
