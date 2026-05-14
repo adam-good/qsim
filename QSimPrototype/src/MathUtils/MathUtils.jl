bee1d899ce6f8edfb3b938d1a02cb926fd2acc41
Adam Good <dev.adamgood@gmail.com>
2026-05-14 16:12:00 -0400

    Fixed include order

--- QSimPrototype/src/MathUtils/MathUtils.jl
+++ QSimPrototype/src/MathUtils/MathUtils.jl
@@ -1,13 +1,15 @@
 module MathUtils
 
-export Scalar, Vector2D, Angle
+export Scalar
+export Vector2D, polar_angle
+export Angle
 
 include("HelperTypes.jl")
-include("Vector2D.jl")
 include("Angle.jl")
+include("Vector2D.jl")
 
 using .HelperTypes: Scalar
-using .VectorUtils: Vector2D
+using .VectorUtils: Vector2D, polar_angle
 using .Angles: Angle
 
 end # module MathUtils
