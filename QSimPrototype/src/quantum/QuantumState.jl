b6409af626e9c2b7cd09a3e27428a69a1a54206c
Adam Good <dev.adamgood@gmail.com>
2026-05-14 22:07:48 -0400

    WIP QState boilerplate

--- QSimPrototype/src/quantum/QuantumState.jl
+++ QSimPrototype/src/quantum/QuantumState.jl
@@ -1,11 +1,28 @@
-module QuantumState
+module QuantumStates
 
-using ..MathUtils: Vector2D
+using ..MathUtils: Vector2D, is_normalized
 
-export State
+export QState
 
-struct State
+"""
+    QState
+
+Quantum State represented as a Vector
+"""
+struct QState
     vec::Vector2D
+
+    QState(vec::Vector2D) = begin
+        if !is_normalized(vec)
+            error("Vector of Quantum State must be normal")
+        end
+        return new(vec)
+    end
 end
+
+qstate_α(state::QState)::Complex = state.vec[1]
+qstate_β(state::QState)::Complex = state.vec[2]
+
+amplitudes(ψ::QState) = (ψ.vec[1], ψ.vec[2])
 
 end # module QuantumState
