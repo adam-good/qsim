a6703ee367618f38eb9dce0fd55fc94b14f704c5
Adam Good <dev.adamgood@gmail.com>
2026-05-25 13:30:50 -0400

    Fixed include / using order

--- QSimPrototype/src/CoreMath/CoreMath.jl
+++ QSimPrototype/src/CoreMath/CoreMath.jl
@@ -3,18 +3,20 @@ module CoreMath
 export Scalar, scalar
 export Vector2D, polar_angle, is_normalized
 export Angle
-export UnitaryMatrix, conjugate_transpose 
+export UnitaryMatrix, conjugate_transpose
 export VectorNotNormalException
-
-using .ScalarUtils: Scalar, scalar
-using .VectorUtils: Vector2D, polar_angle, is_normalized
-using .Angles: Angle
-using .MatrixUtils: UnitaryMatrix, conjugate_transpose
 
 include("Errors.jl")
 include("Scalar.jl")
 include("Angles.jl")
 include("Vectors.jl")
 include("Matrices.jl")
+
+using .ScalarUtils: Scalar, scalar
+using .VectorUtils: Vector2D, polar_angle, is_normalized
+using .Angles: Angle
+using .MatrixUtils: UnitaryMatrix, conjugate_transpose
+
+
 
 end # module CoreMath
