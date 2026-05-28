095f1214096b61472b22627421380ce22dbdfec8
Adam Good <dev.adamgood@gmail.com>
2026-05-28 19:48:38 -0400

    Refactored Scalar

--- QSimPrototype/src/CoreMath/Scalar.jl
+++ QSimPrototype/src/CoreMath/Scalar.jl
@@ -5,6 +5,7 @@ Submodule of CoreMath to define the set of scalars 𝕊 for vector space V
 """
 module ScalarUtils
 
+using Base: isgreater
 export Scalar, scalar
 
 """
@@ -16,42 +17,61 @@ struct Scalar <: Real
     val::Float64 # TODO: Upgrade this to Complex eventually
 end
 
-# TODO: Needs updated when Scalar is upgraded to complex
-Base.Float64(x::Scalar)::Float64 = x.val
-Base.float(x::Scalar)::AbstractFloat = x.val
+# Addition
+Base.:(+)(x::Scalar, y::Scalar) = Scalar(x.val + y.val)
+Base.:(+)(x::Scalar, y::Real)   = Scalar(x.val + y)
+Base.:(+)(x::Real, y::Scalar)   = Scalar(x     + y.val)
 
+# Subtraction
+Base.:(-)(x::Scalar, y::Scalar) = Scalar(x.val - y.val)
+Base.:(-)(x::Scalar, y::Real)   = Scalar(x.val - y)
+Base.:(-)(x::Real, y::Scalar)   = Scalar(x     - y.val)
+
+# Multiplication
+Base.:(*)(x::Scalar, y::Scalar) = Scalar(x.val * y.val)
+Base.:(*)(x::Scalar, y::Real)   = Scalar(x.val * y)
+Base.:(*)(x::Real, y::Scalar)   = Scalar(x     * y.val)
+
+# Division
+Base.:(/)(x::Scalar, y::Scalar) = Scalar(x.val / y.val)
+Base.:(/)(x::Scalar, y::Real)   = Scalar(x.val / y)
+Base.:(/)(x::Real, y::Scalar)   = Scalar(x     / y.val)
+
+# # Power
+# Base.:(^)(x::Scalar, y::Scalar) = Scalar(x.val ^ y.val)
+# Base.:(^)(x::Scalar, y::Real)   = Scalar(x.val ^ y)
+# Base.:(^)(x::Real, y::Scalar)   = Scalar(x     ^ y.val)
+
+# Comparisons
 Base.isapprox(x::Scalar, y::Scalar) = isapprox(x.val, y.val)
-Base.isapprox(x::Scalar, y::AbstractFloat) = isapprox(x.val, y)
-Base.isapprox(x::Scalar, y::Int) = isapprox(x.val, y)
+Base.isapprox(x::Scalar, y::Real)   = isapprox(x.val, y)
+Base.isapprox(x::Real, y::Scalar)   = isapprox(x    , y.val)
 
 Base.:(==)(x::Scalar, y::Scalar) = isapprox(x.val, y.val)
-Base.:(+)(x::Scalar, y::Scalar) = scalar(x.val + y.val)
-Base.:(-)(x::Scalar, y::Scalar) = scalar(x.val - y.val)
-Base.:(*)(x::Scalar, y::Scalar) = scalar(x.val * y.val)
-Base.:(/)(x::Scalar, y::Scalar) = scalar(x.val / y.val)
-Base.:(^)(x::Scalar, y::Scalar) = scalar(x.val ^ y.val)
+Base.:(==)(x::Scalar, y::Real)   = isapprox(x.val, y)
+Base.:(==)(x::Real, y::Scalar)   = isapprox(x    , y.val)
 
-Base.:(==)(x::Scalar, y::Real) = isapprox(x.val, y)
-Base.:(+)(x::Scalar, y::Real) = scalar(x.val + y)
-Base.:(-)(x::Scalar, y::Real) = scalar(x.val - y)
-Base.:(*)(x::Scalar, y::Real) = scalar(x.val * y)
-Base.:(/)(x::Scalar, y::Real) = scalar(x.val / y)
-# Base.:(^)(x::Scalar, y::Real) = scalar(x.val^y)
+Base.isless(x::Scalar, y::Scalar) = isless(x.val, y.val)
+Base.isless(x::Scalar, y::Real)   = isless(x.val, y)
+Base.isless(x::Real, y::Scalar)   = isless(x    , y.val)
 
-Base.:(==)(x::Real, y::Scalar) = isapprox(x.val, y)
-Base.:(+)(x::Real, y::Scalar) = scalar(x + y.val)
-Base.:(-)(x::Real, y::Scalar) = scalar(x - y.val)
-Base.:(*)(x::Real, y::Scalar) = scalar(x * y.val)
-Base.:(/)(x::Real, y::Scalar) = scalar(x / y.val)
-# Base.:(^)(x::Real, y::Scalar) = scalar(x^y.val)
+Base.isgreater(x::Scalar, y::Scalar) = isgreater(x.val, y.val)
+Base.isgreater(x::Scalar, y::Real)   = isgreater(x.val, y)
+Base.isgreater(x::Real, y::Scalar)   = isgreater(x    , y.val)
 
+# Identities
+Base.one(::Scalar)  = Scalar(1)
 Base.zero(::Scalar) = Scalar(0)
+
+# Transformations
 Base.transpose(x::Scalar) = x
 Base.conj(x::Scalar) = x # TODO: This must change when Scalar is upgraded to complex
 
+# Conversions
 Base.convert(::Type{Scalar}, x::Int64) = Scalar(x)
 Base.convert(::Type{Scalar}, x::Float64) = Scalar(x)
 
+# Display Functions
 Base.show(io::IO, x::Scalar) = print(io, "$(x.val)")
 Base.show(io::IO, w::AbstractVector{Scalar}) = begin
     for x in w
